2a98e472fbb5ce902eea90f4531146792aead6e9
Doug Lauver <admin@clicknip.com>
2014-02-17 09:03:39 -0600

    Add index file to serve as content

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Hello World Capistrano | cap-win-clean</title>
+	</head>
+	<body>
+		<h1>Hello World Capistrano!</h1>
+		<h2>Project: cap-win-clean</h2>
+    <p>This is an example of a "project" that will not be deployed in a web accessible folder.</p>
+	</body>
+</html>
